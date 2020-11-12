6cd320dd8bb0ca71cda902318901948375d8d178
Muhidin <muhidinhukic1@gmail.com>
2020-11-12 09:44:32 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,11 @@
 # Enter your procedural solution here!
+    
+
+def collect_multiples(limit)
+    (1...limit).select {|nums| nums % 3 == 0 || nums % 5 == 0}
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+
+
+class Multiples
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        (1...@limit).select {|nums| nums % 3 == 0 || nums % 5 == 0}
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+end
